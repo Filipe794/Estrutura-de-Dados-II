0350e41e4b0200c1e936067b357b87be97f0748d
Filipe794 <mendesfilipe794@gmail.com>
2023-07-01 11:30:51 -0300

    ED II

--- Arvores/Arvore B+/b+tree.cpp
+++ Arvores/Arvore B+/b+tree.cpp
@@ -1,290 +1,424 @@
-#include <stdio.h>
-#include <stdlib.h>
+// NÃO FUNCIONA
 
-#define T 5
 
-typedef struct
-{
-  int codigo_barra;
-  char nome[15];
-  char data[10];
-  int quant;
-  float preco;
-} medicamento;
 
-typedef struct page
-{
-  int pos = 0;
-  bool folha = false;
-  int codigos[(2 * T) - 1];
-  struct page *pai = NULL;
-  medicamento *medicamentos[(2 * T) - 1];
-  struct page *filhos[2 * T];
-  struct page *prox = NULL;
-} pagina;
 
-pagina *root = NULL;
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <iostream>
+// using namespace std;
 
-pagina *criar_pagina();
+// #define T 3
 
-bool folha(pagina *pag)
-{
-  for (int i = 0; i <= pag->pos + 1; i++)
-  {
-    if (pag->filhos[i] != NULL)
-    {
-      return false;
-    }
-  }
-  return true;
-}
+// typedef struct
+// {
+//   string codigo_barra;
+//   string nome;
+//   string data;
+//   int quant;
+//   float preco;
+// } medicamento;
 
-void imprimir_medicamento(medicamento *med)
-{
-  printf("Codigo de barras: %d\n", med->codigo_barra);
-  printf("Nome: %s\n", med->nome);
-  printf("Data do Cadastro: %s\n", med->data);
-  printf("Quantidade: %d\n", med->quant);
-  printf("Preço: %f\n", med->preco);
-}
+// typedef struct page
+// {
+//   int pos = 0;
+//   bool folha = false;
+//   string codigos[T - 1];
+//   struct page *pai = NULL;
+//   medicamento *medicamentos[T - 1];
+//   struct page *filhos[T];
+//   struct page *prox = NULL;
+// } pagina;
 
-void print(pagina *pag)
-{
-  if (pag == NULL)
-    return;
+// pagina *criar_pagina();
+// void inserir_na_folha(pagina **root, medicamento *med);
 
-  for (int i = 0; i < pag->pos; i++)
-  {
-    if (!folha(pag))
-    {
-      print(pag->filhos[i]);
-    }
-    imprimir_medicamento(pag->medicamentos[i]);
-  }
-  print(pag->filhos[pag->pos]);
-}
+// void imprimir_medicamento(medicamento *med)
+// {
+//   cout << "Codigo de barras: " << med->codigo_barra << endl;
+//   printf("Nome: %s\n", med->nome);
+//   printf("Data do Cadastro: %s\n", med->data);
+//   printf("Quantidade: %d\n", med->quant);
+//   printf("Preço: %f\n", med->preco);
+// }
 
-medicamento *criar_medicamento()
-{
-  medicamento *med = new medicamento;
+// void print_medicamentos(pagina *pag)
+// {
+//   if (pag == NULL)
+//     return;
 
-  printf("Insira o codigo de barra do medicamento: ");
-  scanf("%d", &med->codigo_barra);
+//   if (pag->folha)
+//   {
+//     for (int i = 0; i < pag->pos; i++)
+//     {
+//       imprimir_medicamento(pag->medicamentos[i]);
+//       printf("\n");
+//     }
+//     print_medicamentos(pag->prox);
+//   }
+//   print_medicamentos(pag->filhos[0]);
+// }
 
-  printf("Insira o nome do medicamento: ");
-  scanf("%s", med->nome);
+// void printBPlusTree(pagina *root)
+// {
+//   if (root != NULL)
+//   {
+//     printf("[");
+//     int i;
+//     for (i = 0; i < root->pos; i++)
+//     {
+//       cout << root->codigos[i] << " ";
+//     }
+//     printf("]");
+//     if (!(root->folha))
+//     {
+//       cout << "->" << endl;
+//       for (i = 0; i < root->pos + 1; i++)
+//       {
+//         printBPlusTree(root->filhos[i]);
+//         if (i < root->pos)
+//           cout << root->codigos[i] << " ";
+//       }
+//     }
+//   }
+// }
 
-  printf("Insira a data do cadastro: ");
-  scanf("%s", med->data);
+// medicamento *criar_medicamento()
+// {
+//   medicamento *med = new medicamento;
 
-  printf("Insira a quantidade do medicamento: ");
-  scanf("%d", &med->quant);
+//   printf("Insira o codigo de barra do medicamento: ");
+//   cin >> med->codigo_barra;
 
-  printf("Insira o preco do medicamento: ");
-  scanf("%f", &med->preco);
+//   printf("Insira o nome do medicamento: ");
+//   scanf("%s", med->nome);
 
-  return med;
-}
+//   printf("Insira a data do cadastro: ");
+//   scanf("%s", med->data);
 
-pagina *dividir_pagina(pagina *esq)
-{
-  int meio = ((2 * T) - 1) / 2;
+//   printf("Insira a quantidade do medicamento: ");
+//   scanf("%d", &med->quant);
 
-  pagina *pai, *dir;
+//   printf("Insira o preco do medicamento: ");
+//   scanf("%f", &med->preco);
 
-  pai = criar_pagina();
-  dir = criar_pagina();
+//   return med;
+// }
 
-  // atribuindo os filhos ao pai
-  pai->filhos[0] = esq;
-  pai->filhos[1] = dir;
+// pagina *dividir_pagina(pagina *esq)
+// {
 
-  // encadeando a lista dos vetores das folhas
-  dir->prox = esq->prox;
-  esq->prox = dir;
+//   int meio = (T - 1) / 2;
 
-  for (int i = 0; i < (2 * T - 1) - (meio + 1); i++)
-  {
-    dir->medicamentos[i] = esq->medicamentos[i + (meio + 1)];
-    dir->pos++;
+//   pagina *pai, *dir;
+//   // tratar caso pai!=NULL
+//   pai = criar_pagina();
+//   dir = criar_pagina();
 
-    esq->medicamentos[i + (meio + 1)] = NULL;
-    esq->pos--;
-  }
+//   // atribuindo os filhos ao pai
+//   pai->filhos[0] = esq;
+//   pai->filhos[1] = dir;
 
-  // adicionando o codigo de barra da esq nos codigos do pai
-  pai->codigos[0] = esq->medicamentos[meio]->codigo_barra;
-  pai->pos++;
+//   // encadeando a lista dos vetores das folhas
+//   dir->prox = esq->prox;
+//   esq->prox = dir;
 
-  esq->medicamentos[meio] = NULL;
-  esq->pos--;
+//   int i;
+//   for (i = 0; i < (T - 1) - (meio + 1); i++)
+//   {
+//     dir->medicamentos[i] = esq->medicamentos[i + (meio + 1)];
+//     dir->filhos[i] = esq->filhos[i + (meio + 1)];
+//     dir->pos++;
 
-  esq->pai = pai;
-  dir->pai = pai;
+//     esq->medicamentos[i + (meio + 1)] = NULL;
+//     esq->filhos[i + (meio + 1)] = NULL;
+//     esq->pos--;
+//   }
 
-  return pai;
-}
+//   dir->filhos[i] = esq->filhos[i + (meio + 1)];
+//   esq->filhos[i + (meio + 1)] = NULL;
 
-void inserir_pagina(pagina **pg, medicamento *med)
-{
-  pagina *pag = busca_insercao(root, med->codigo_barra);
+//   // adicionando o codigo de barra da esq nos codigos do pai
+//   pai->codigos[0] = esq->medicamentos[meio]->codigo_barra;
+//   pai->pos++;
 
-  if (folha(pag))
-  {
-    if (pag->pos < (2 * T) - 1)
-    {
-      int i = pag->pos - 1;
-      while (i >= 0 && pag->medicamentos[i]->codigo_barra > med->codigo_barra)
-      {
-        if (med->codigo_barra == pag->medicamentos[i]->codigo_barra)
-        {
-          pag->medicamentos[i]->quant++;
-          printf("Medicamento já existe. Quantidade incrementada em 1.\n");
-          return;
-        }
-        pag->medicamentos[i + 1] = pag->medicamentos[i];
-        i--;
-      }
-      pag->medicamentos[i + 1] = med;
-      pag->pos++;
-    }
-    else
-    {
-      *pg = dividir_pagina(pag);
-      inserir_pagina(pg, med);
-    }
-  }
-  else
-  {
-    int i = 0;
-    
-    for (int i = 0; i < pag->pos; i++)
-    {
-      if (med->codigo_barra == pag->medicamentos[i]->codigo_barra)
-      {
-        pag->medicamentos[i]->quant++;
-        return;
-      }
-      if (med->codigo_barra < pag->medicamentos[i]->codigo_barra)
-      {
-        inserir_pagina(&pag->filhos[i], med);
-      }
-    }
-    inserir_pagina(&pag->filhos[pag->pos], med);
-  }
-}
+//   pai->pai = esq->pai;
+//   esq->pai = dir->pai = pai;
 
-pagina *criar_pagina()
-{
-  pagina *pag = new pagina;
-  for (int i = 0; i < 2 * T - 1; i++)
-  {
-    pag->codigos[i] = 0;
-    pag->medicamentos[i] = NULL;
-    pag->filhos[i] = NULL;
-  }
-  pag->filhos[2 * T] = NULL;
-  return pag;
-}
+//   pai->folha = false;
+//   if (esq->folha)
+//     dir->folha = true;
 
-pagina *inserir_folha(pagina *folha, medicamento *med)
-{
-  int i, inserir;
+//   return pai;
+// }
 
-  inserir = 0;
-  while (inserir < folha->pos && folha->medicamentos[inserir]->codigo_barra < med->codigo_barra)
-    inserir++;
+// void inserir_pai(pagina **root, pagina *pag, pagina *pai)
+// {
+//   if ((*root) == NULL)
+//   {
+//     return;
+//   }
+//   if (pai == NULL)
+//   {
+//     *root = pag;
+//     return;
+//   }
 
-  for (i = folha->pos; i > inserir; i--)
-  {
-    folha->medicamentos[i] = folha->medicamentos[i - 1];
-    folha->filhos[i] = folha->filhos[i - 1];
-  }
-  folha->medicamentos[inserir] = med;
-  folha->pos++;
-  return folha;
-}
+//   if (pai->pos < T - 1)
+//   {
+//     int i = pai->pos - 1;
+//     while (i >= 0 && pag->codigos[0] < pai->codigos[i])
+//     {
+//       pai->codigos[i + 1] = pai->codigos[i];
+//       if (i == pai->pos - 1)
+//       {
+//         pai->filhos[i + 2] = pai->filhos[i + 1];
+//       }
+//       pai->filhos[i + 1] = pai->filhos[i];
+//       i--;
+//     }
+//     pai->codigos[i + 1] = pag->codigos[0];
+//     pai->filhos[i + 1] = pag->filhos[0];
+//     pai->filhos[i + 2] = pag->filhos[1];
+//     pai->pos++;
+//     return;
+//   }
+//   else
+//   {
+//     pagina *new_pai = dividir_pagina(pai);
+//     inserir_pai(root, pai, new_pai);
+//   }
+// }
 
-void inserir(pagina **root, medicamento *med)
-{
-  if (*root == NULL)
-  {
-    (*root) = criar_pagina();
-    (*root)->codigos[0] = med->codigo_barra;
-    (*root)->medicamentos[0] = med;
-    (*root)->pos++;
-    (*root)->folha = true;
-  }
-  else
-  {
-    inserir_pagina(root, med);
-  }
-}
+// pagina *busca_insercao(pagina *root, string codigo_barra)
+// {
+//   if (root == NULL)
+//   {
+//     return NULL;
+//   }
+//   int i = 0;
 
-pagina *busca_insercao(pagina *root, int codigo_barra)
-{
-  if (root == NULL)
-  {
-    return NULL;
-  }
-  int i = 0;
+//   while (i < root->pos && codigo_barra > root->codigos[i])
+//     i++;
 
-  while (i < root->pos && codigo_barra > root->codigos[i])
-    i++;
+//   // medicamento ja existe na arvore
+//   if (root->folha)
+//   {
+//     if (codigo_barra == root->codigos[i])
+//     {
+//       root->medicamentos[i]->quant++;
+//       printf("Medicamento já existe. Quantidade incrementada em 1.\n");
+//       return NULL;
+//     }
+//     return root;
+//   }
+//   return busca_insercao(root->filhos[i], codigo_barra);
+// }
 
-  if (i < root->pos && codigo_barra == root->codigos[i])
-  {
-    // medicamento ja existe na arvore
-    root->medicamentos[i]->quant++;
-    printf("Medicamento já existe. Quantidade incrementada em 1.\n");
-    return NULL;
-  }
+// bool verifica_irmao_espaco(pagina *pag)
+// {
+//   if (pag->prox = NULL)
+//   {
+//     return false;
+//   }
 
-  if (root->folha)
-  {
-    return root;
-  }
+//   if ((pag->prox->pos < T - 1) && (pag->prox->pai == pag->pai))
+//   {
+//     return true;
+//   }
+//   return false;
+// }
 
-  return busca_insercao(root->filhos[i], codigo_barra);
-}
+// int pai_index_filho(pagina *pai, pagina *filho)
+// {
+//   int i;
+//   for (i = 0; i < pai->pos + 1; i++)
+//   {
+//     if (pai->filhos[i] == filho)
+//     {
+//       return i;
+//     }
+//   }
+//   return -1;
+// }
 
-pagina *busca(pagina *pag, int codigo_barra)
-{
-  if (pag == NULL)
-  {
-    return NULL;
-  }
+// void inserir_na_folha(pagina **root, medicamento *med)
+// {
+//   if (*root == NULL)
+//   {
+//     return;
+//   }
+//   // retorna uma folha pra realizar uma inserção
+//   pagina *pag = busca_insercao(*root, med->codigo_barra);
+//   // verifica se a pagina retornada nao é nula
+//   if (pag == NULL)
+//   {
+//     return;
+//   }
+//   // verifica se o medicamento ja existe na folha
+//   int i = pag->pos - 1; // começa do ultimo elemento do vetor
+//   while (i >= 0 && pag->medicamentos[i]->codigo_barra > med->codigo_barra)
+//   {
+//     if (med->codigo_barra == pag->medicamentos[i]->codigo_barra)
+//     {
+//       pag->medicamentos[i]->quant++;
+//       printf("Medicamento já existe. Quantidade incrementada em 1.\n");
+//       return;
+//     }
+//     i--;
+//   }
+//   // se tem espaço para inserir
+//   if (pag->pos < T - 1)
+//   {
+//     int i = pag->pos - 1;
+//     while (i >= 0 && pag->medicamentos[i]->codigo_barra > med->codigo_barra)
+//     {
+//       pag->medicamentos[i + 1] = pag->medicamentos[i];
+//       i--;
+//     }
+//     pag->medicamentos[i + 1] = med;
+//     pag->pos++;
+//     return;
+//   }
+//   else // n tem espaço
+//   {
+//     // verificar se o prox tem espaço e é do msm pai
+//     // fazer uma outra função de inserção que verifica se o pai tem espaço
+//     // se n tiver, divide e insere no pai do pai e por ai vai
+//     // caso irmao com espaço e possui o mesmo pai
+//     if (verifica_irmao_espaco(pag))
+//     {
+//       int index = pai_index_filho(pag->pai, pag);
+//       if (pag->medicamentos[pag->pos - 1]->codigo_barra < med->codigo_barra)
+//       {
+//         // jogar a ultima posição pra pai
+//         pag->pai->codigos[index] = pag->medicamentos[pag->pos - 1]->codigo_barra;
+//         inserir_na_folha(root, med);
+//         return;
+//       }
+//       else
+//       {
+//         medicamento *aux = pag->medicamentos[pag->pos - 1];
+//         pag->pos--;
+//         inserir_na_folha(root, med);
+//         pag->pai->codigos[index] = pag->medicamentos[pag->pos - 1]->codigo_barra;
+//         inserir_na_folha(root, aux);
+//         return;
+//       }
+//     }
+//     else
+//     {
+//       pagina *pai = dividir_pagina(pag);
+//       inserir_na_folha(&pai, med);
+//       inserir_pai(root, pai, pai->pai);
+//     }
+//   }
+// }
 
-  if (folha(pag))
-  {
-    for (int i = 0; i < pag->pos; i++)
-    {
-      if (codigo_barra == pag->medicamentos[i]->codigo_barra)
-      {
-        return pag;
-      }
-    }
-    return NULL;
-  }
+// pagina *criar_pagina()
+// {
+//   pagina *pag = new pagina;
+//   for (int i = 0; i < 2 * T - 1; i++)
+//   {
+//     pag->codigos[i] = '\0';
+//     pag->medicamentos[i] = NULL;
+//     pag->filhos[i] = NULL;
+//   }
+//   pag->filhos[2 * T] = NULL;
+//   return pag;
+// }
 
-  for (int i = 0; i < pag->pos; i++)
-  {
-    if (codigo_barra <= pag->codigos[i])
-    {
-      return busca(pag->filhos[i], codigo_barra);
-    }
-  }
-  busca(pag->filhos[pag->pos], codigo_barra);
-  return NULL;
-}
+// void inserir(pagina **root, medicamento *med)
+// {
+//   if (*root == NULL)
+//   {
+//     (*root) = criar_pagina();
+//     (*root)->codigos[0] = med->codigo_barra;
+//     (*root)->medicamentos[0] = med;
+//     (*root)->pos++;
+//     (*root)->folha = true;
+//   }
+//   else
+//   {
+//     inserir_na_folha(root, med);
+//   }
+// }
 
-int main()
-{
-  medicamento *med = criar_medicamento();
-  imprimir_medicamento(med);
-  printf("\n");
-  inserir(&root, med);
-  printf("\n");
-  print(root);
-  return 0;
-}
+// pagina *busca(pagina *pag, string codigo_barra)
+// {
+//   if (pag == NULL)
+//   {
+//     return NULL;
+//   }
+//   if (pag->folha)
+//   {
+//     for (int i = 0; i < pag->pos; i++)
+//     {
+//       if (codigo_barra == pag->medicamentos[i]->codigo_barra)
+//       {
+//         return pag;
+//       }
+//     }
+//     return NULL;
+//   }
+//   for (int i = 0; i < pag->pos; i++)
+//   {
+//     if (codigo_barra <= pag->codigos[i])
+//     {
+//       return busca(pag->filhos[i], codigo_barra);
+//     }
+//   }
+//   busca(pag->filhos[pag->pos], codigo_barra);
+//   return NULL;
+// }
+
+// void menu()
+// {
+//   printf("\nMENU\n");
+//   printf("1- Cadastrar Participante\n");
+//   printf("2- Buscar Participante\n");
+//   printf("3- Sortear\n");
+//   printf("4- Imprimir Participantes\n");
+//   printf("0-Sair\n");
+// }
+
+// int main()
+// {
+//   pagina *root = NULL;
+//   medicamento *med = criar_medicamento();
+//   inserir(&root, med);
+//   printBPlusTree(root);
+//   // med = criar_medicamento();
+//   // inserir(&root, med);
+//   // med = criar_medicamento();
+//   // inserir(&root, med);
+//   // imprimir_medicamento(med);
+//   // printf("\n");
+//   // printf("\n");
+//   print_medicamentos(root);
+
+//   int opc = -1;
+//   while (opc != 0)
+//   {
+//     menu();
+//     scanf("%d", &opc);
+//     switch (opc)
+//     {
+//     case 1:
+//       break;
+//     case 2:
+//       break;
+//     case 3:
+//       break;
+//     case 4:
+//       break;
+//     case 0:
+//       break;
+//     default:
+//       printf("Opção inválida\n");
+//       break;
+//     }
+//   }
+
+//   return 0;
+// }
